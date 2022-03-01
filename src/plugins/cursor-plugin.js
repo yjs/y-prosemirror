cf7e08c63df285c4e02e2c73cd1a15c7f0702f25
Andreas Kohn <andreas.kohn@gmail.com>
2022-03-01 10:27:13 +0100

    Fix the JSDoc for `yCursorPlugin`
    This fixes the typing of the plugin construction as well, and removes the need to specify
    a useless `cursorStateField` in the options.

--- src/plugins/cursor-plugin.js
+++ src/plugins/cursor-plugin.js
@@ -80,7 +80,7 @@ export const createDecorations = (state, awareness, createCursor) => {
  * @param {object} [opts]
  * @param {function(any):HTMLElement} [opts.cursorBuilder]
  * @param {function(any):any} [opts.getSelection]
- * @param {string} [opts.cursorStateField] By default all editor bindings use the awareness 'cursor' field to propagate cursor information.
+ * @param {string} [cursorStateField] By default all editor bindings use the awareness 'cursor' field to propagate cursor information.
  * @return {any}
  */
 export const yCursorPlugin = (awareness, { cursorBuilder = defaultCursorBuilder, getSelection = state => state.selection } = {}, cursorStateField = 'cursor') => new Plugin({
