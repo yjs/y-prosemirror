bad9fb8631bc9a1236877fbc9515d109d6afbf6c
Nick the Sick <computers@nickthesick.com>
2026-03-18 15:06:10 +0100

    feat: position mapping API

--- src/sync/positions.js
+++ src/sync/positions.js
@@ -32,15 +32,15 @@ export const absolutePositionToRelativePosition = (pos, type, pmDoc, am = Y.noAt
 
 /**
  * Transforms a {@link Y.RelativePosition} to a Prosemirror based absolute position.
- * @param {Y.Doc} y
- * @param {Y.XmlFragment} documentType Top level type that is bound to pView
  * @param {Y.RelativePosition} relPos Encoded Yjs based relative position
+ * @param {Y.XmlFragment} documentType Top level type that is bound to pView
  * @param {import('prosemirror-model').Node} pmDoc
+ * @param {Y.AbstractAttributionManager} [am]
  * @return {null|number} Prosemirror based absolute position
  */
-export const relativePositionToAbsolutePosition = (y, documentType, relPos, pmDoc) => {
+export const relativePositionToAbsolutePosition = (relPos, documentType, pmDoc, am = Y.noAttributionsManager) => {
   // (1) decodedPos.index is the absolute position starting at the referred  prosemirror node.
-  const decodedPos = Y.createAbsolutePositionFromRelativePosition(relPos, y)
+  const decodedPos = Y.createAbsolutePositionFromRelativePosition(relPos, documentType.doc, undefined, am)
   if (decodedPos === null || (decodedPos.type !== documentType && !Y.isParentOf(documentType, decodedPos.type._item))) {
     return null
   }
@@ -51,6 +51,7 @@ export const relativePositionToAbsolutePosition = (y, documentType, relPos, pmDo
    * result = (1) + (2)
    */
   const path = Y.getPathTo(documentType, decodedPos.type)
+  // TODO what if the ytype is a grandchild of the documentType? I think this assumes a direct child relationship
   let pos = 1 // Start inside the document
   let currentNode = pmDoc
   // Traverse the path to find the nested position
@@ -66,4 +67,73 @@ export const relativePositionToAbsolutePosition = (y, documentType, relPos, pmDo
   }
   // Add the offset within the target node
   return pos + decodedPos.index
+}
+
+/**
+ * Creates a function that can be used to keep track of an absolute position of a Prosemirror document, and restore it to an absolute position in a different Prosemirror document.
+ * @param {number} pos Absolute position in the Prosemirror document
+ * @param {Y.XmlFragment} type Top level type that is bound to pView
+ * @param {import('prosemirror-model').Node} pmDoc Prosemirror document
+ * @param {Y.AbstractAttributionManager} [am] Attribution manager to use for the relative position
+ * @returns {(doc: import('prosemirror-model').Node, documentType?: Y.XmlFragment, attributionManager?: Y.AbstractAttributionManager) => number}
+ */
+export const relativePositionStore = (pos, type, pmDoc, am) => {
+  const relPos = absolutePositionToRelativePosition(pos, type, pmDoc, am)
+
+  return (doc, documentType = type, attributionManager) => {
+    return relativePositionToAbsolutePosition(relPos, documentType, doc, attributionManager)
+  }
+}
+
+/**
+ *
+ * @param {Y.XmlFragment} type
+ * @param {import('prosemirror-model').Node} pmDoc
+ * @param {Y.AbstractAttributionManager} [am]
+ * @returns {{captureMapping: import('prosemirror-transform').Mappable, restoreMapping: (type: Y.XmlFragment, pmDoc: import('prosemirror-model').Node, am?: Y.AbstractAttributionManager) => import('prosemirror-transform').Mappable}}
+ */
+export const relativePositionStoreMapping = (type, pmDoc, am) => {
+  /**
+   * @type {Map<number, Y.RelativePosition>}
+   */
+  const positionMapping = new Map()
+
+  return {
+    captureMapping: (clear = false) => {
+      if (clear) {
+        positionMapping.clear()
+      }
+      return {
+        map (pos) {
+          // Store the relative position using the position as the key
+          positionMapping.set(pos, absolutePositionToRelativePosition(pos, type, pmDoc, am))
+
+          // Pass through the position unchanged, since we are just using it to store the relative position
+          return pos
+        },
+        mapResult (pos) {
+          // Call the map function to store the relative position
+          return { pos: this.map(pos), deleted: false, deletedAcross: false, deletedAfter: false, deletedBefore: false }
+        }
+      }
+    },
+    restoreMapping (type, pmDoc, am) {
+      return {
+        map (pos) {
+          const relPos = positionMapping.get(pos)
+          if (!relPos) {
+            throw new Error('Relative position not set')
+          }
+          return relativePositionToAbsolutePosition(relPos, type, pmDoc, am)
+        },
+        mapResult (originalPos) {
+          const mappedPos = this.map(originalPos)
+          if (mappedPos === null) {
+            return { pos: originalPos, deleted: true, deletedAcross: true, deletedAfter: true, deletedBefore: true }
+          }
+          return { pos: mappedPos, deleted: false, deletedAcross: false, deletedAfter: false, deletedBefore: false }
+        }
+      }
+    }
+  }
 }
