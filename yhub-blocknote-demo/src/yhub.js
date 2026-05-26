982010e046ef8f318f2c7610af253a914e703023
Nick the Sick <computers@nickthesick.com>
2026-05-26 21:42:45 +0200

    chore: lint comma

--- yhub-blocknote-demo/src/yhub.js
+++ yhub-blocknote-demo/src/yhub.js
@@ -107,7 +107,7 @@ export const mapAttributionToMark = (format, attribution) => {
     const userIds = [...(new Set(Object.values(attribution.format).flat()))]
     out['y-attributed-format'] = {
       id: userIds[0] ?? null,
-      'user-color': colorForUserIds(userIds),
+      'user-color': colorForUserIds(userIds)
     }
   }
   return out
